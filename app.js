0d0cde85477ac57a557ee37f212f9f2e84a3f9b6
Moshe <azachi.moshiko@gmail.com>
2023-06-24 02:48:59 +0300

    Create user model,and authorization controller for users signup

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const shopRouter = require('./routes/shopRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -15,5 +17,11 @@ app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/items', shopRouter);
 app.use('/api/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on the server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
