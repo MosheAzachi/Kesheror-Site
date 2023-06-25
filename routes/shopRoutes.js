4a96b4853492b3133c218b9c577fdd133de25b88
Moshe <azachi.moshiko@gmail.com>
2023-06-25 22:54:09 +0300

    start groupBy on sales

--- routes/shopRoutes.js
+++ routes/shopRoutes.js
@@ -5,6 +5,8 @@ const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
+router.route('/items-on-sale').get(shopController.getItemsOnSale);
+
 router.route('/').get(authController.protect, shopController.getAllItems).post(shopController.createItem);
 
 router
